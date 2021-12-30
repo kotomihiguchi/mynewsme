7a90833c2fa599ad79ebdeefbe7e1a3172fc3c99
sample2 <koto.higuchi.hw93@gmail.com>
2021-12-30 04:53:56 +0000

    9-resubmission

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,17 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
+});
+
+
+//「http://XXXXXX.jp/XXX というアクセスが来たときに、 AAAControllerのbbbというAction に渡すRoutingの設定」
+Route::get('xxx', 'AAAController@bbb');
+
+//Admin/ProfileControllerを作成し、add Action, edit Actionを追加しました。web.phpを編集して、
+//admin/profile/create にアクセスしたら ProfileController の add Action に、
+//admin/profile/edit にアクセスしたら ProfileController の edit Action に割り当てるように設定してください
+Route::get('admin/profile/create', 'ProfileController@add');
+Route::get('admin/profile/edit', 'ProfileController@edit');
