a41bb3f7c25dcb00ad6f71be295121b290fe24bb
sample2 <koto.higuchi.hw93@gmail.com>
2022-01-04 13:20:31 +0000

    12-submission

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,9 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
-    Route::get('profile/create', 'Admin\ProfileController@add');
-    Route::get('profile/edit', 'Admin\ProfileController@edit');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
 });
 
 
@@ -27,3 +27,7 @@ Route::get('xxx', 'AAAController@bbb');
 
 
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
