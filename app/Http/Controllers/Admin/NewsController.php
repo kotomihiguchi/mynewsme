2c5b088a13417cd9a7ddbf24522e428646cffa1e
sample2 <koto.higuchi.hw93@gmail.com>
2022-01-10 08:28:48 +0000

    15

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -25,7 +25,7 @@ class NewsController extends Controller
       
       $news = new News;
       $form = $request->all();
-      
+      \Debugbar::info($form);
       // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
       if (isset($form['image'])) {
       
@@ -46,5 +46,20 @@ class NewsController extends Controller
       
       // admin/news/createにリダイレクトする
       return redirect('admin/news/create');
-  }  
+  } 
+  
+  // 以下を追記
+  public function index(Request $request)
+  {
+      $cond_title = $request->cond_title;
+      if ($cond_title != '') {
+          // 検索されたら検索結果を取得する
+          $posts = News::where('title', $cond_title)->get();
+      } else {
+          // それ以外はすべてのニュースを取得する
+          $posts = News::all();
+      }
+      return view('admin.news.index', ['posts' => $posts, 'cond_title' => $cond_title]);
+  }
+
 }
--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Profile;
 
 class ProfileController extends Controller
 {
@@ -27,4 +28,19 @@ class ProfileController extends Controller
     {
         return redirect('admin/profile/edit');
     }
+    // 以下を追記
+  public function index(Request $request)
+  {
+      $cond_title = $request->cond_title;
+      if ($cond_title != '') {
+          // 検索されたら検索結果を取得する
+          $posts = Profile::where('name', $cond_title)->get();
+      } else {
+          // それ以外はすべてのニュースを取得する
+          $posts = Profile::all();
+      }
+      return view('admin.profile.index', ['posts' => $posts, 'cond_title' => $cond_title]);
+  }
+
+
 }
